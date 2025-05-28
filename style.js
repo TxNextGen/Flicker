0f97b5f6bef2d26bcffa83c8c1032b4850606742
Vander <160795443+txvander@users.noreply.github.com>
2025-05-27 22:40:03 -0500

    Update style.js

--- style.js
+++ style.js
@@ -1,4 +1,1 @@
-document.getElementById("input").addEventListener("input", function () {
-    this.style.height = "80px"; // Default height
-    this.style.height = Math.min(this.scrollHeight, 120) + "px"; // Expands but stays contained
-});
+
