8b24ecd5d090fd8fd5e835f0ae3e386b5de6d093
Bliss <bliss@purelymail.com>
2025-07-10 16:53:32 -0400

    better security

--- backend/config.py
+++ backend/config.py
@@ -1,10 +1,10 @@
-API_KEY = "AIzaSyACMaC6ESMCIk-4AMpMkN7BKegCSxMIHwE" #Limited, cause i am not paying money for this. If you get a error dm me.
+API_KEY = "shouldbekeptsecret" # use an environment file to securely store the api keys
 MEMORY_FILE = "memory.json"
 MODEL_NAME = "gemini-2.5-flash" # Changed Version should work now 
 MAX_HISTORY = 10
 
 GENERATION_CONFIG = {
-"safety_settings": [
+"safety_settings": [ # optimal and default safety settings
 {"category": "HARM_CATEGORY_HARASSMENT", "threshold": "BLOCK_ALL"},
 {"category": "HARM_CATEGORY_HATE_SPEECH", "threshold": "BLOCK_ALL"},
 {"category": "HARM_CATEGORY_SEXUALLY_EXPLICIT", "threshold": "BLOCK_ALL"},
